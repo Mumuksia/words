fa560a2858f57fe9555c89f7e7653b2a895fd18c
Yurko <yuriy.zavada@benify.com>
2016-04-24 23:41:22 +0200

    start point jaxrs

---
+++ src/main/java/com/muksia/config/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.muksia.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Muksia
+ */
+@ApplicationPath("app")
+public class JAXRSConfiguration extends Application{
+    
+}
